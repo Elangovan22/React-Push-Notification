48e20af86cbdf4420c7f8162c62d59667acffc2e
Elangovan <elangshoba@gmail.com>
2025-01-31 22:08:00 +0530

    patch update

--- android/app/.cxx/Debug/4l1o271p/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/elangovaneaaswaramurty/.gradle/caches/8.9/transforms/8837ea610f4e2c5e62c533c3d9af5486/transformed/hermes-android-0.77.0-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/elangovaneaaswaramurty/.gradle/caches/8.9/transforms/8837ea610f4e2c5e62c533c3d9af5486/transformed/hermes-android-0.77.0-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
